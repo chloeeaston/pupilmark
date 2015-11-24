bfa6ac4b12e30559c350c9c9f8aaa192162c80ce
Guest User <Guest@stk-cs01-ws13.westlothian.sch.uk>
2015-11-24 10:24:51 +0000

    sprint 1.3
    created properties.

--- PUPIL.java
+++ PUPIL.java
@@ -1,8 +1,11 @@
 public class PUPIL
 {
+    //properties
     
-    
-    
+    private String fName;
+    private String sName; 
+    private int pupilmark;
+   
    public PUPIL()
     {
         //constuctor 
