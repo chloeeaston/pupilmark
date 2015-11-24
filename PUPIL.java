0ac8a457a00a5826478961aa120b80db1d0e8658
Guest User <Guest@stk-cs01-ws13.westlothian.sch.uk>
2015-11-24 10:25:07 +0000

    Revert "sprint 1.3"
    This reverts commit bfa6ac4b12e30559c350c9c9f8aaa192162c80ce.

--- PUPIL.java
+++ PUPIL.java
@@ -1,11 +1,8 @@
 public class PUPIL
 {
-    //properties
     
-    private String fName;
-    private String sName; 
-    private int pupilmark;
-   
+    
+    
    public PUPIL()
     {
         //constuctor 
