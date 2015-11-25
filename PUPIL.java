7d1d1c90d735b57ee98b4cd9c0c86bb806277fe5
chloeeaston <Guest@stk-cs01-ws13.westlothian.sch.uk>
2015-11-25 12:18:11 +0000

    Sprint 1.4

--- PUPIL.java
+++ PUPIL.java
@@ -3,12 +3,37 @@ public class PUPIL
     // list of prperties
     private String fname;
     private String sname;
-    private int pupilmark;
-    
-    
-   public PUPIL()
+    private Float pupilmark;
+
+    public PUPIL()
     {
         //constuctor 
+
+        fname = "";
+        sname = "";
+        pupilmark = 0.0f; 
+
     }
-    
+    // file handling store details from file 
+    public void readPupilDetails (String dataItems)
+    { // unpack string of row data into fields 
+        String [] rowItems = dataItems.split(",");
+        // store each data items as instance property 
+        fname = rowItems [0]; 
+        sname = rowItems [1];
+        pupilmark = Float.parseFloat(rowItems[2]);
+    }
+
+    public String writeDetails ()
+    {
+        // Join up data into a string to output as a row
+        // use "," to seperate csv colums 
+        String pupilData = "";
+        pupilData = pupilData.concat(fname);
+        pupilData = pupilData.concat(",");
+        pupilData = pupilData.concat(sname);
+        pupilData = pupilData.concat(",");
+        pupilData = pupilData.concat(Float.toString(pupilmark));
+        return pupilData;
+    }
 }
