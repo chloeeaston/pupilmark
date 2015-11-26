f7a79c863ff435ed6e5702abedce5ddecc290519
chloeeaston <Guest@stk-cs01-ws13.westlothian.sch.uk>
2015-11-26 09:38:49 +0000

    sprint 2.1
    created SCHOOL class

---
+++ SCHOOL.java
@@ -0,0 +1,29 @@
+public class SCHOOL
+{
+    // array of PUPIL objects
+    private PUPIL pupilList[]; 
+    // number of pupils to be called
+    int noOfPupils;
+
+    public SCHOOL()
+    {
+
+    }
+
+    // top level algorithm
+    public void processpupils()
+    { 
+        setupPupilList();
+        countpupilmark();
+
+    }
+
+    private void setupPupilList()
+    { // placeholder
+        
+    }
+    public void countpupilmark()
+    {
+        //placeholder
+    }
+}
