bcb342ed3964cb73de8b1c7b19f9f03b8d6f198e
chloeeaston <Guest@stk-cs01-ws13.westlothian.sch.uk>
2015-12-07 14:58:02 +0000

    Sprint 5

--- SCHOOL.java
+++ SCHOOL.java
@@ -5,11 +5,10 @@ public class SCHOOL
     // array of PUPIL objects
     private PUPIL pupilList[]; 
     private int maxDataPosition ;
-    
+
     // number of pupils to be called
     int noOfPupils;
     FILEREADCSV pupilmark;
-    
 
     public SCHOOL()
     { pupilmark = new FILEREADCSV(); 
@@ -20,47 +19,42 @@ public class SCHOOL
     public void processpupils() throws IOException
     { 
         setupPupilList();
-        countpupilmark();
-        
+        findmaxData();
 
     }
-
     public void setupPupilList() throws IOException 
-
     { // placeholder
         System.out.println("School : pupilmark update");
         System.out.println("** Preparing to read data file.");
         String [] dataRows = pupilmark.readCSVtable ();
-        noOfPupils = dataRows.length -1;
+        noOfPupils = dataRows.length ;
         System.out.println("** " + noOfPupils + " rows read.\n\n");
         pupilList = new PUPIL [noOfPupils];
 
         for (int i = 0; i < noOfPupils; i++) {
             pupilList[i]= new PUPIL ();
-            pupilList[i].readPupilDetails(dataRows[i+1]);
+            pupilList[i].readPupilDetails(dataRows[i]);
 
         }
     }
-    public void findmaxDataPosition ()
-    {
-        int maxDataPosition = 0;
-    } 
+
     
-
-    public void countpupilmark() throws IOException
-    {
-        //placeholder
-        for (int i = 0; i < noOfPupils; i ++)
+    public void findmaxData() {
         {
-            if (pupilList[i].getpupilmark() < pupilList [maxDataPosition].getpupilmark())  {
-                maxDataPosition = 1; 
-            }
+            //placeholder
+            int maxDataPosition = 0;
+            
+            for (int i = 0; i < noOfPupils; i ++)
+            {
+                if (pupilList[i].getpupilmark() > pupilList [maxDataPosition].getpupilmark())  {
+                    maxDataPosition = i; 
+                }
 
-        } 
-        System.out.print("Topmark is:" + maxDataPosition );
+            } 
+            System.out.print("Topmark is: " + maxDataPosition );
             pupilList[maxDataPosition].displayDetails();
             System.out.println(); 
         }
     }
-
+}
     
