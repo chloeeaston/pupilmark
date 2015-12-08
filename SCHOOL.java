a04acfb7d1672040c7ee56ea595a872ca118d1cc
Guest User <Guest@stk-cs01-ws13.westlothian.sch.uk>
2015-12-08 10:32:52 +0000

    SPRINT 5.0

--- SCHOOL.java
+++ SCHOOL.java
@@ -9,9 +9,12 @@ public class SCHOOL
     // number of pupils to be called
     int noOfPupils;
     FILEREADCSV pupilmark;
+    FILEWRITECSV resultFile;
+    private int topmark;
 
     public SCHOOL()
     { pupilmark = new FILEREADCSV(); 
+        resultFile = new FILEWRITECSV();
 
     }
 
@@ -22,6 +25,7 @@ public class SCHOOL
         findmaxData();
 
     }
+
     public void setupPupilList() throws IOException 
     { // placeholder
         System.out.println("School : pupilmark update");
@@ -38,23 +42,32 @@ public class SCHOOL
         }
     }
 
-    
-    public void findmaxData() {
-        {
-            //placeholder
-            int maxDataPosition = 0;
-            
-            for (int i = 0; i < noOfPupils; i ++)
-            {
-                if (pupilList[i].getpupilmark() > pupilList [maxDataPosition].getpupilmark())  {
-                    maxDataPosition = i; 
-                }
+    public void findmaxData()  throws IOException 
+    {
+        //placeholder
+        int maxDataPosition = 0;
+        String FileContent = "";
 
+        for (int i = 0; i < noOfPupils; i ++)
+        {
+            if (pupilList[i].getpupilmark() > pupilList [maxDataPosition].getpupilmark())  {
+                maxDataPosition = i; 
+            }
+            if (maxDataPosition>1)
+            { 
+                FileContent = FileContent.concat("\n");
+                
             } 
+            FileContent = FileContent.concat(pupilList[1].writeDetails());
+
             System.out.print("Topmark is: " + maxDataPosition );
             pupilList[maxDataPosition].displayDetails();
             System.out.println(); 
+
+            System.out.println ("** preparing to write data file. ");
+            resultFile.writeCSVtable(FileContent);
+            System.out.println(" File written and closed.");
         }
     }
 }
-    
+
---
+++ __SHELL9.java
@@ -0,0 +1,10 @@
+
+public class __SHELL9 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("/Users/Guest/Documents/pupilmark");
+final SCHOOL sCHOOL1 = (SCHOOL)__bluej_runtime_scope.get("sCHOOL1");
+
+
+sCHOOL1.processpupils();
+
+}}
